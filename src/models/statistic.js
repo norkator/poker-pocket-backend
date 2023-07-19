ba5e44116a21029515a4f2a4d199d36339d1dd03
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2023-07-19 20:11:52 +0300

    add statistic table to user relation

--- src/models/statistic.js
+++ src/models/statistic.js
@@ -11,7 +11,13 @@ module.exports = (sequelize, type) => {
       primaryKey: true,
       autoIncrement: true
     },
-    user_id: type.BIGINT,
+    user_id: {
+      type: type.BIGINT,
+      references: {
+        model: 'users', // refers to table name
+        key: 'id', // refers to column name in model value table
+      }
+    },
     money: type.NUMERIC,
     win_count: type.INTEGER,
     lose_count: type.INTEGER,
